f4ead0a5cd2caf15b91046b8cb7dea1d3914fe26
Hussain Ali <76788374+hussain301@users.noreply.github.com>
2023-12-23 11:54:24 +0500

    Update folder_paths.py

--- folder_paths.py
+++ folder_paths.py
@@ -5,8 +5,8 @@ supported_pt_extensions = set(['.ckpt', '.pt', '.bin', '.pth', '.safetensors'])
 
 folder_names_and_paths = {}
 
-base_path = 'kaggle/temp/'
-models_dir = os.path.join(base_path, "models")
+base_path = os.path.dirname(os.path.realpath(__file__))
+models_dir = os.path.join('/kaggle/temp/', "models")
 folder_names_and_paths["checkpoints"] = ([os.path.join(models_dir, "checkpoints")], supported_pt_extensions)
 folder_names_and_paths["configs"] = ([os.path.join(models_dir, "configs")], [".yaml"])
 
